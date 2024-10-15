6b098be0065d5cd4765a516964ea3c7679e7c8a8
peligro99 <cristianespinel95@gmail.com>
2024-10-14 22:31:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Header />
+   <Routes>
+    <Route path="/" exact={true} element={<Home />}/>
+   </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
